bf979b2ebc9bd02636fc55534dfba15ee0c0d9ce
tim <tim.chan0931@gmail.com>
2024-04-28 21:53:17 -0400

    remove target, as it is not needed

--- target/compiled/tipico/models/tipico/group.sql
+++
@@ -1,6 +0,0 @@
-
-
-select distinct *
-,SYSDATE as ads_dtm_created
-,SYSDATE as ads_dtm_last_updated
-from "dev"."timothy_chan"."group_raw_data_test"
--- target/compiled/tipico/models/tipico/outcome.sql
+++
@@ -1,6 +0,0 @@
-
-
-select distinct *
-,SYSDATE as ads_dtm_created
-,SYSDATE as ads_dtm_last_updated
-from "dev"."timothy_chan"."outcome_raw_data"

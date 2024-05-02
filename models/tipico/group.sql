b1cfaabd8ccfc9d7b37cd3f19c536a738b86f4a2
tim <tim.chan0931@gmail.com>
2024-05-02 00:16:59 -0400

    upload tipico_data_pull zip and csvs

--- models/tipico/group.sql
+++ models/tipico/group.sql
@@ -1,6 +1,15 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='table',
+    unique_key='group_id',
+    strategy='merge',
+    merge_update_keys=['name','parentgroupname','parentgroupid'],
+    merge_behavior={
+        'when_matched': 'update',
+        'when_not_matched': 'insert'
+    }
+) }}
 
 select distinct *
 ,SYSDATE as ads_dtm_created
 ,SYSDATE as ads_dtm_last_updated
-from {{ref("group_raw_data_test")}}
+from {{ref("group_raw_data")}}

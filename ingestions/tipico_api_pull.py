d7d859f90dd97c066c09daf08c74ec1505851f8c
tim <tim.chan0931@gmail.com>
2024-05-02 13:57:01 -0400

    use psycopg2 to write data to redshift staging table

--- ingestions/tipico_api_pull.py
+++ ingestions/tipico_api_pull.py
@@ -5,6 +5,40 @@ import pandas as pd
 import boto3
 from botocore.exceptions import ClientError
 import zipfile
+import psycopg2
+import logging
+from sqlalchemy import create_engine
+
+def create_redshift_tables(df,table_name,schema):
+     # Define the Redshift connection parameters
+    dbname = 'dev'
+    schema = 'timothy_chan'
+    host = 'manual-dwh-candidatetests.258845600139.us-east-1.redshift-serverless.amazonaws.com'
+    port = '5439'
+    user = 'timothy_chan'
+    password = 'POr0410!!__djif24r2fdsmbj'
+
+    # Establish a connection to Redshift using psycopg2
+    conn = psycopg2.connect(
+        dbname=dbname,
+        host=host,
+        port=port,
+        user=user,
+        password=password
+    )
+    logging.basicConfig()
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+    # Create a SQLAlchemy engine to be used by pandas. Use echo parameter to show SQL query
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{dbname}',echo=True)
+    
+    # Truncate staging table, so we don't need to drop it
+    cursor = conn.cursor()
+    cursor.execute(f"""TRUNCATE {schema}.{table_name}""")
+
+    # Insert data from dataframe to table
+    df.to_sql(table_name,engine,index=False,if_exists='append',schema=schema)
+
+    conn.close()
 
 '''
 Parameters:
@@ -26,54 +60,69 @@ def parse_json_response(response,region,filename,bucket,key):
     for item in response:
         # Iterate through participants[] and append each JSON object to participant_lst
         for p in item.get('participants','NA'):
-                    participant_lst.append({'id': p.get('id','NA')
+                    participant_lst.append({ 'root_id': item.get('id','NA')
+                                            ,'participant_id': p.get('id','NA')
                                             ,'name': p.get('name','NA')
                                             ,'position': p.get('position','NA')
                                             ,'abbreviation': p.get('abbreviation','NA')})
         for m in item['markets']:
             # Iterate through markets[] and append each JSON object to market_lst
-            market_lst.append({ 'id': m.get('id','NA')
-                            ,'startTime': item.get('startTime','2999-12-31 00:00:00')
-                            ,'messageTime': item.get('messageTime','2999-12-31 00:00:00')
-                            ,'matchState': item.get('matchState','NA')
-                            ,'sportType': item.get('sportType','NA')
+            market_id = m.get('id','NA')
+            print(market_id)
+            market_lst.append({'root_id': item.get('id','NA')
+                            ,'market_id': m.get('id','NA')
+                            ,'start_Time': item.get('startTime','2999-12-31 00:00:00')
+                            ,'message_Time': item.get('messageTime','2999-12-31 00:00:00')
+                            ,'match_State': item.get('matchState','NA')
+                            ,'sport_Type': item.get('sportType','NA')
                             ,'name': m.get('name','NA')
                             ,'type': m.get('type','NA')
-                            ,'parameters': m.get('parameters',[])
+                            ,'parameters': str(m.get('parameters','[]'))
                             ,'status': m.get('status','NA')
-                            ,'mostBalancedLine': m.get('mostBalancedLine',False)
-                            ,'sgpEligable': m.get('sgpEligable',False)})
+                            ,'most_Balanced_Line': m.get('mostBalancedLine',False)
+                            ,'is_sgp_Eligable': m.get('sgpEligable',False)})
             # Iterate through outcomes[] and append each JSON object to outcome_lst
             for outcome in m.get('outcomes','NA'):
-                outcome_lst.append({'id': outcome.get('id','NA')
+                outcome_lst.append({'root_id': item.get('id','NA')
+                                    ,'market_id': market_id,
+                                    'outcome_id': outcome.get('id','NA')
                                     ,'name': outcome.get('name','NA')
-                                    ,'isTraded': outcome.get('isTraded','NA')
-                                    ,'trueOdds': outcome.get('trueOdds',0.0)
-                                    ,'formatDecimal': outcome.get('formatDecimal',0.0)
-                                    ,'formatAmerican': outcome.get('formatAmerican',0.0)
+                                    ,'traded_ind': outcome.get('isTraded','NA')
+                                    ,'true_Odds': outcome.get('trueOdds',0.0)
+                                    ,'format_Decimal': outcome.get('formatDecimal',0.0)
+                                    ,'format_American': outcome.get('formatAmerican',0.0)
                                     ,'status': outcome.get('status','NA')
-                                    ,'trueOdds': outcome.get('trueOdds',0.0)})
+                                    ,'true_Odds': outcome.get('trueOdds',0.0)})
         # Iterate through eventDetails[] and append each JSON object to eventDetails_lst
-        eventDetails_lst.append({'block_cashout': item.get('eventDetails','NA').get('block_cashout','NA')
-                            ,'longTermEventType': item.get('eventDetails','NA').get('longTermEventType','NA')
-                            ,'outrightType': item.get('eventDetails','NA').get('outrightType','NA')
-                            ,'subgroupNameKey': item.get('eventDetails','NA').get('subgroupNameKey','NA')
-                            ,'subgroupIdKey': item.get('eventDetails','NA').get('subgroupIdKey',-9999)
-                            ,'tiebreak': item.get('eventDetails','NA').get('tiebreak','NA')
+        eventDetails_lst.append({'root_id': item.get('id','NA')
+                            ,'event_name': item.get('eventName','NA')
+                            ,'block_cashout': item.get('eventDetails','NA').get('block_cashout','NA')
+                            ,'long_Term_Event_Type': item.get('eventDetails','NA').get('longTermEventType','NA')
+                            ,'outright_Type': item.get('eventDetails','NA').get('outrightType','NA')
+                            ,'sub_group_Name_Key': item.get('eventDetails','NA').get('subgroupNameKey','NA')
+                            ,'sub_group_Id_Key': item.get('eventDetails','NA').get('subgroupIdKey',-9999)
+                            ,'tie_break': item.get('eventDetails','NA').get('tiebreak','NA')
                             ,'best_of_sets': item.get('eventDetails','NA').get('best_of_sets','NA')})
         # Iterate through group[] and append each JSON object to group_lst
-        group_lst.append({'id': item.get('group','NA').get('id',-9999)
+        group_lst.append({'root_id': item.get('id','NA')
+                            ,'group_id': item.get('group','NA').get('id',-9999)
                             ,'name': item.get('group','NA').get('name','NA')
-                            ,'parentGroupName': item.get('group','NA').get('parentGroup','NA').get('name','NA')
-                            ,'parentGroupId': item.get('group','NA').get('parentGroup','NA').get('id','NA')})
+                            ,'parent_Group_Name': item.get('group','NA').get('parentGroup','NA').get('name','NA')
+                            ,'parent_Group_Id': item.get('group','NA').get('parentGroup','NA').get('id','NA')})
 
     # Create a Pandas DataFrame from all the lists and convert them to a CSV file. Don't include the index and quote all columns
-    pd.DataFrame(outcome_lst).to_csv('./seeds/outcome_raw_data.csv',index=False,quoting=csv.QUOTE_ALL)
-    pd.DataFrame(group_lst).to_csv('./seeds/group_raw_data.csv',quoting=csv.QUOTE_ALL,index=False)
-    pd.DataFrame(eventDetails_lst).to_csv('./seeds/event_dtls_raw_data.csv',quoting=csv.QUOTE_ALL,index=False)
-    pd.DataFrame(participant_lst).to_csv('./seeds/participants_raw_data.csv',quoting=csv.QUOTE_ALL,index=False)
-    pd.DataFrame(specifier_lst).to_csv('./seeds/specifier_raw_data.csv',quoting=csv.QUOTE_ALL,index=False)
-    pd.DataFrame(market_lst).to_csv('./seeds/market_raw_data.csv',quoting=csv.QUOTE_ALL,index=False)
+    # pd.DataFrame(outcome_lst).to_csv('./seeds/outcome_raw_data.csv',index=False,quoting=csv.QUOTE_ALL)
+    create_redshift_tables(pd.DataFrame(market_lst),'market_raw_data','timothy_chan')
+    create_redshift_tables(pd.DataFrame(outcome_lst),'outcome_raw_data','timothy_chan')
+    # pd.DataFrame(group_lst).to_csv('./seeds/group_raw_data.csv',quoting=csv.QUOTE_ALL,index=False)
+    # pd.DataFrame(eventDetails_lst).to_csv('./seeds/event_dtls_raw_data.csv',quoting=csv.QUOTE_ALL,index=False)
+    create_redshift_tables(pd.DataFrame(eventDetails_lst),'event_dtls_raw_data','timothy_chan')
+    # pd.DataFrame(participant_lst).to_csv('./seeds/participants_raw_data.csv',quoting=csv.QUOTE_ALL,index=False)
+    create_redshift_tables(pd.DataFrame(participant_lst),'participants_raw_data','timothy_chan')
+    # pd.DataFrame(specifier_lst).to_csv('./seeds/specifier_raw_data.csv',quoting=csv.QUOTE_ALL,index=False)
+    # create_redshift_tables(pd.DataFrame(specifier_lst),'specifier','timothy_chan') 
+    # pd.DataFrame(market_lst).to_csv('./seeds/market_raw_data.csv',quoting=csv.QUOTE_ALL,index=False)
+  
 
     # Zip up all the CSVs
     with zipfile.ZipFile('tipico_data_pull.zip',mode='w') as tipico_data_pull:
@@ -86,7 +135,7 @@ def parse_json_response(response,region,filename,bucket,key):
 
     # Upload zip to S3 bucket
     s3_client = boto3.client(service_name='s3',region_name=region)
-    s3_client.upload_file(Filename=filename,Bucket=bucket,Key=key)         
+    s3_client.upload_file(Filename=filename,Bucket=bucket,Key=key)
 
 def main():
     #API URL
@@ -98,6 +147,7 @@ def main():
     #Make a GET request to API URL and store the response in response 
     response = requests.request("GET", url, headers=headers, data=payload)
     # Only parse JSON Object if status is OK
+    print(response.status_code)
     if response.status_code == 200:
         content_type = response.headers['Content-Type']
         # For now, we are only expecting to get a JSON array/object from the API response

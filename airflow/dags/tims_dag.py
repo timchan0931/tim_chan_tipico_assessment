b1e40358502a743107aea87beb8b6665fb08a9ff
tim <tim.chan0931@gmail.com>
2024-05-05 00:17:36 -0400

    navigate to parent directory in order to run dbt

--- airflow/dags/tims_dag.py
+++ airflow/dags/tims_dag.py
@@ -22,55 +22,55 @@ dag = DAG('tipico',
 # Pull Tipico Data from API and populate staging tables
 pull_tipico_api_and_populate_stage = BashOperator(
     task_id='run_tipico_script',
-    bash_command='/usr/local/bin/python3 /Users/tim/tipico_git/ingestions/tipico_api_pull.py' ,
+    bash_command='python3 ../tipico_git/ingestions/tipico_api_pull.py' ,
     dag=dag
 )
 
 # Populate dim_event
 dbt_dim_event = BashOperator(
     task_id='run_dbt_dim_event',
-    bash_command='cd /Users/tim/tipico_git && dbt run --select dim_event' ,
+    bash_command='cd .. dbt run --select dim_event' ,
     dag=dag
 )
 
 # Populate dim_participant
 dbt_dim_participant = BashOperator(
     task_id='run_dbt_dim_participant',
-    bash_command='cd /Users/tim/tipico_git && dbt run --select dim_participant' ,
+    bash_command='cd .. && dbt run --select dim_participant' ,
     dag=dag
 )
 
 # Populate dim_event_dtls
 dbt_dim_event_dtls = BashOperator(
     task_id='run_dbt_event_dtls',
-    bash_command='cd /Users/tim/tipico_git && dbt run --select dim_event_dtls' ,
+    bash_command='cd .. && dbt run --select dim_event_dtls' ,
     dag=dag
 )
 
 # Populate dim_group
 dbt_dim_group= BashOperator(
     task_id='run_dbt_group',
-    bash_command='cd /Users/tim/tipico_git && dbt run --select dim_group' ,
+    bash_command='cd .. && dbt run --select dim_group' ,
     dag=dag
 )
 
 # Populate dim_outcome
 dbt_dim_outcome= BashOperator(
     task_id='run_dbt_outcome',
-    bash_command='cd /Users/tim/tipico_git && dbt run --select dim_outcome' ,
+    bash_command='cd .. && dbt run --select dim_outcome' ,
     dag=dag
 )
 
 # Populate dim_market
 dbt_dim_market= BashOperator(
     task_id='run_dbt_market',
-    bash_command='cd /Users/tim/tipico_git && dbt run --select dim_market' ,
+    bash_command='cd .. && dbt run --select dim_market' ,
     dag=dag
 )
 # Populate dim_specifier
 dbt_dim_specifier= BashOperator(
     task_id='run_dbt_specifier',
-    bash_command='cd /Users/tim/tipico_git && dbt run --select dim_specifier' ,
+    bash_command='cd .. && dbt run --select dim_specifier' ,
     dag=dag
 )
 
